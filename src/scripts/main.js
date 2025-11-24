6616cd3459a726e607eb5e8d673bfead6d339c81
Evan Alford <evan.al4d@gmail.com>
2025-11-24 11:50:52 -0600

    added console logs to check makePottery function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,19 @@
 // Imports go first
-
+import { makePottery } from "./PotteryWheel.js"
 
 
 // Make 5 pieces of pottery at the wheel
-let mug = makePottery()
+const mug = makePottery("Mug", 2, 4);
+const platter = makePottery("Platter", 8, 1);
+const bowl = makePottery("Bowl", 3, 3);
+const vase = makePottery("Vase", 5, 9);
+const plate = makePottery("Plate", 6, 2);
 
-
+console.log(mug);
+console.log(platter);
+console.log(bowl);
+console.log(vase);
+console.log(plate);
 // Fire each piece of pottery in the kiln
 
 
